2a455ae2c96a4be0913da3a877fa7c8a5b5a7d65
vlad-matyunin96 <vlad.matyunin96@gmail.com>
2017-09-27 11:53:55 +0300

    speed up added

--- MergeSort/src/ru/vladmatyunin/Main.java
+++ MergeSort/src/ru/vladmatyunin/Main.java
@@ -5,6 +5,8 @@ import ru.vladmatyunin.mergesort.ParallelSortExecutor;
 
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Main {
 
@@ -14,21 +16,49 @@ public class Main {
         int size = scanner.nextInt();
         System.out.println("Generate?(y/n)");
         String generate = scanner.next();
+        int[] array;
         if (generate.toLowerCase().equals("y")){
-            int[] generatedArray = MyArraysUtils.generate(size);
-            int[] sortedArray = new ParallelSortExecutor().parallelSort(generatedArray);
-            System.out.println(Arrays.toString(sortedArray));
+            array = MyArraysUtils.generate(size);
+
         }
         else {
             System.out.println("Write array:");
-            int[] array = new int[size];
+            array = new int[size];
             for (int i = 0; i < size; i++) {
                 array[i] = scanner.nextInt();
             }
-            //MergeSorter mergeSorter = new MergeSorter();
-            //int[] sortedArray = mergeSorter.sort(array);
-            int[] sortedArray = new ParallelSortExecutor().parallelSort(array);
-            System.out.println(Arrays.toString(sortedArray));
         }
+        executeSpeedUp(array);
+    }
+    private static void executeSpeedUp(int[] array){
+        try {
+
+            long timeParallel = executeSpeedUp(array, true);
+            long time = executeSpeedUp(array, false);
+            System.out.println("NOT PARALLEL: " + time);
+            System.out.println("PARALLEL: " + timeParallel);
+        }
+        catch (Exception e){
+            System.out.println(e.toString());
+        }
+
+    }
+    private static long executeSpeedUp(int[] array, boolean parallel) throws InterruptedException {
+        long timeStart = System.nanoTime();
+        execute(array, parallel);
+        long timeEnd = System.nanoTime();
+        return timeEnd - timeStart;
+    }
+    private static void execute(int[] array, boolean parallel) throws InterruptedException {
+        //System.out.println(Arrays.toString(array));
+        int[] sortedArray = null;
+        if (parallel){
+            sortedArray = new ParallelSortExecutor().parallelSort(array);
+        }
+        else {
+            MergeSorter mergeSorter = new MergeSorter();
+            sortedArray = mergeSorter.sort(array);
+        }
+        //System.out.println(Arrays.toString(sortedArray));
     }
 }
